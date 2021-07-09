986dbca253c5164e1dabc2b89a129430e0249f80
plwu1024 <b07901022@ntu.edu.tw>
2021-07-09 08:24:33 +0000

    1

--- backend/src/routes/todo.ts
+++ backend/src/routes/todo.ts
@@ -12,6 +12,41 @@ const TodoRouter = (server: FastifyInstance, opts: RouteShorthandOptions, done: 
 
     // TODO: Add CRUD endpoints, i.e. get, post, update, delete
     // NOTE: the url should be RESTful
+    server.get('/todos', opts, async (request, reply)=>{
+        try {
+            const todos: Array<ITodo> = await todoRepo.getTodos()
+            return reply.status(200).send({todos})
+        } catch (error) {
+            console.error(`GET /todos Error: ${error}`)
+            return reply.status(500).send(`[server error]: ${error}`)
+        }
+    })
+
+    server.post('/todos', opts, async (request, reply) => {
+        try {
+            const todoIn: ITodo = request.body as ITodo
+            const todoOut: ITodo = await todoRepo.addTodo(todoIn)
+            return reply.status(200).send(todoOut)
+        } catch (error) {
+            console.error(`GET /todos Error: ${error}`)
+            return reply.status(500).send(`[server error]: ${error}`)
+        }
+    })
+    server.put<{ Params: IdParam }>('/todos/:id', opts, async (request, reply) => {
+        try {
+            const id = request.params.id
+            const todoIn: ITodo = request.body as ITodo
+            const todoOut: ITodo | null = await todoRepo.updateTodo(id, todoIn)
+            if (todoOut != null){
+                return reply.status(200).send(todoOut)
+            }
+            console.error(`PUT /todos Error: todo not found`)
+            return reply.status(404).send(todoOut)
+        } catch (error) {
+            console.error(`PUt /todos Error: ${error}`)
+            return reply.status(500).send(`[server error]: ${error}`)
+        }
+    })
 
     done()
 }
--- backend/src/test/todo.spec.ts
+++ backend/src/test/todo.spec.ts
@@ -31,7 +31,82 @@ describe('Form test', () => {
                 (reason) => new Error(`Failed to close a Fastify server, reason: ${reason}`)
             )
         )
-    })    
+    })
 
     // TODO: Add some test cases like CRUD, i.e. get, post, update, delete
+    it("should get all todos", async () => {
+        const response = await server.inject({ method: 'GET', url: '/api/todos' })
+        expect(response.statusCode).toBe(200)
+        expect(response.body).toStrictEqual(JSON.stringify({ todos: [] }))
+    })
+
+    it("should add a todo item", async () => {
+        const response = await server.inject({
+            method: 'POST', url: '/api/todos', payload: {
+                name: "test_todo",
+                description: "this is a test todo.",
+                status: false
+            }
+        })
+        const response2 = await server.inject({
+            method: 'GET', url: '/api/todos'
+        })
+        expect(response.statusCode).toBe(200)
+        expect(response2.statusCode).toBe(200)
+        expect(response2.body).toStrictEqual(JSON.stringify({
+            todos: [
+                JSON.parse(response.body)
+            ]
+        }))
+    })
+
+    it("should add a todo item", async () => {
+        const response = await server.inject({
+            method: 'POST', url: '/api/todos', payload: {
+                name: "test_todo",
+                description: "this is a test todo.",
+                status: false
+            }
+        })
+        const testTodo : ITodo = JSON.parse(response.body)
+        expect(response.statusCode).toBe(200)
+
+        const response2 = await server.inject({
+            method: 'GET', url: '/api/todos'
+        })
+        // expect(response.body).toBe("")
+        expect(response2.statusCode).toBe(200)
+        expect(response2.body).toStrictEqual(JSON.stringify({
+            todos: [
+                testTodo
+            ]
+        }))
+        const response3 = await server.inject({
+            method: 'PUT', url: `/api/todos:id=${testTodo._id}`, payload: {
+                name: 'modified_test_todo',
+                description: 'this is a modified test todo.',
+                status: true
+            }
+        })
+        const modifiedTodo : ITodo = JSON.parse(response3.body)
+        expect(modifiedTodo).toStrictEqual({
+            _id: testTodo._id,
+            name: 'modified_test_todo',
+            description: 'this is a modified test todo.',
+            status: true
+        })
+        // expect(modifiedTodo._id).toBe(testTodo._id)
+        // // expect(modifiedTodo).toBe("3")
+        // expect(modifiedTodo.name).toBe('modified_test_todo')
+        // expect(modifiedTodo.description).toBe('this is a modified test todo.')
+        // expect(modifiedTodo.status).toBe(true)
+        // 
+        expect(response3.body).toStrictEqual(JSON.stringify({
+            todos: [
+                modifiedTodo
+            ]
+        }))
+    })
+
+
 })
